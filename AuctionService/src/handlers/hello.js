8426609173723fc2fffa917c38f306f1ede3ac90
alishkeir <ali.shkeir@itxi.net>
2022-11-17 14:16:54 +0200

    Deploying to SLS AWS

--- AuctionService/src/handlers/hello.js
+++ AuctionService/src/handlers/hello.js
@@ -1,10 +1,8 @@
 async function hello(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
-  };
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Hello! This is Ali Shkeir' }),
+    };
 }
 
 export const handler = hello;
-
-
